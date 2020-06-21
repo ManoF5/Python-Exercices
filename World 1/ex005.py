e2a939bd9ed9a7d1c831dbf3679ff7b4e3c33fe4
Manoel Fernandes <manoelfelipe2000@hotmail.com>
2020-06-21 14:07:14 -0300

    New ex (006) modify (005)

--- World 1/ex005.py
+++ World 1/ex005.py
@@ -1,4 +1,4 @@
 # INPUT
-n = int(input("Type a number: "))
+num = int(input("Type a number: "))
 # OUTPUT
-print("Analyzing the value {} the predecessor is {} and successor is {}".format(n, (n-1), (n+1)))
+print("Analyzing the value {} the predecessor is {} and successor is {}".format(num, (num-1), (num+1)))
---
+++ World 1/ex006.py
@@ -0,0 +1,9 @@
+import math
+# INPUT
+num = int(input("Type a number: "))
+# MATH
+double = num * 2
+triple = num * 3
+square_root = math.sqrt(num)
+# OUTPUT
+print("\nThe number: {} \nThe double: {} \nThe Triple: {} \nThe square root: {:.2f}".format(num, double, triple, square_root))
