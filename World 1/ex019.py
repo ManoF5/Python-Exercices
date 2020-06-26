504cbae4fc57fbe2c61563b85653c92e11a31cbe
Manoel Fernandes <manoelfelipe2000@hotmail.com>
2020-06-26 14:43:50 -0300

    modify ex(019)

--- World 1/ex019.py
+++ World 1/ex019.py
@@ -1,10 +1,10 @@
-import random
+from random import choice
 # INPUT
 n1 = str(input("First student: "))
 n2 = str(input("Second student: "))
 n3 = str(input("Third student: "))
 n4 = str(input("Fourth student: "))
 namelist = [n1, n2, n3, n4]
-choice = random.choice(namelist)
+choice = choice(namelist)
 # OUTPUT
 print("The chosen student was {}".format(choice))
