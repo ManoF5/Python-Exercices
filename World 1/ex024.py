9a973add0b443daa4c4d10a8fbd50a3fff1cb188
Manoel Fernandes <manoelfelipe2000@hotmail.com>
2020-07-01 17:24:22 -0300

    new ex (024,025)

---
+++ World 1/ex024.py
@@ -0,0 +1,4 @@
+# INPUT
+city = str(input('What city were you born in? ')).strip()
+# OUTPUT
+print(city[:5].upper() == 'SANTO')
---
+++ World 1/ex025.py
@@ -0,0 +1,4 @@
+# INPUT
+name = str(input('Type your full name: ')).strip()
+# OUTPUT
+print('\nHave "Silva" in the name? {} \n'.format('SILVA' in name.upper()))
