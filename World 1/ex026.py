4c5e8cb7ea9e1c4e915b06eebf7b0924fe89409c
Manoel Fernandes <manoelfelipe2000@hotmail.com>
2020-07-01 19:08:10 -0300

    Update ex026.py

--- World 1/ex026.py
+++ World 1/ex026.py
@@ -1,5 +1,5 @@
 # INPUT
-phrase = str((input('Type a phrase: ')).upper()).strip()
+phrase = str(input('Type a phrase: ')).upper().strip()
 # OUTPUT
 print('Times the letter "A" appears:    \t \t{}'.format(phrase.count('A')) )
 print('Position of the first letter "A":\t \t{}'.format(phrase.find('A') + 1) )
