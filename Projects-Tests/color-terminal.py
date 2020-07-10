021a53cb367c4be98ab4a677eb8f8306366a3e57
Manoel Fernandes <manoelfelipe2000@hotmail.com>
2020-07-10 20:03:28 -0300

    Create color-terminal.py

---
+++ Projects-Tests/color-terminal.py
@@ -0,0 +1,48 @@
+#   STYLE
+print('\n')
+# none
+print('\033[0m Hello World! \033[m')
+# bold
+print('\033[1m Hello World! \033[m')
+# negative 
+print('\033[7m Hello World! \033[m')
+# underline
+print('\033[4m Hello World! \033[m')
+
+#   TEXT COLOR
+print('\n')
+# black
+print('\033[30m Hello world! \033[m')
+# red
+print('\033[31m Hello world! \033[m')
+# green
+print('\033[32m Hello world! \033[m')
+# yellow
+print('\033[33m Hello World! \033[m')
+# blue
+print('\033[34m Hello World! \033[m')
+# purple
+print('\033[35m Hello World! \033[m')
+# cyan
+print('\033[36m Hello World! \033[m')
+# gray
+print('\033[37m Hello World! \033[m')
+
+#   BACKGROUND COLOR
+print('\n')
+# none
+print('\033[40m Hello World! \033[m')
+# red
+print('\033[41m Hello World! \033[m')
+# green
+print('\033[42m Hello World! \033[m')
+# yellow
+print('\033[43m Hello World! \033[m')
+# blue
+print('\033[44m Hello World! \033[m')
+# purple
+print('\033[45m Hello World! \033[m')
+# cyan
+print('\033[46m Hello World! \033[m')
+# gray
+print('\033[47m Hello World! \033[m')
