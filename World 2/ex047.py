4328698a639d2763b3a9316957fffaf94a7f31e7
Manoel Fernandes <manoelfelipe2000@hotmail.com>
2020-07-21 19:20:14 -0300

    new ex.
    new(46,47,48)

---
+++ World 2/ex047.py
@@ -0,0 +1,5 @@
+# Even numbers from 0 to 50
+for i in range(0, 51):
+    if i % 2 == 0:
+        print(i)
+print('END')
---
+++ World 2/ex048.py
@@ -0,0 +1,5 @@
+# Odd numbers and multiples of 3 from 0 to 500
+for i in range(0 , 501, 3):
+    if i % 2 != 0:
+        print(i)
+print('END')
