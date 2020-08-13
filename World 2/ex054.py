a58c65f553f954d4cc60e63dba26ec5a57dd3aa0
Manoel Fernandes <manoelfelipe2000@hotmail.com>
2020-08-13 18:16:32 -0300

    modifications
    mod [ 54 - 55 ]

--- World 2/ex054.py
+++ World 2/ex054.py
@@ -1,9 +1,12 @@
+from datetime import date
 # DATA
+current_year = date.today().year
 minority = 0
 adulthood = 0
 # INPUT
-for i in range(0, 6):
-    age = int(input('Type your birth year: '))
+for i in range(0, 7):
+    birth_year = int(input('Type the birth year of the {}° person: '.format(i+1)))
+    age = current_year - birth_year
     if age < 18: 
         minority += 1
     else:
--- World 2/ex055.py
+++ World 2/ex055.py
@@ -5,18 +5,22 @@ colors = {
 }
 # DATA
 hi_weight = 0
-low_weight = 1000
+low_weight = 0
 # INTRO
 print('='*27)
 print('{} Highest and Lowest Weight {}'.format(colors['GREEN'], colors['CLEAN']))
 print('='*27)
 # INPUT
 for i in range(0,5):
-    weight = float(input('Type your weight: ')) 
-    if weight > hi_weight:
+    weight = float(input('Type the weight of the {}° person: '.format(i+1))) 
+    if i == 1:
         hi_weight = weight
-    if weight < low_weight:
         low_weight = weight
+    else:
+        if weight > hi_weight:
+            hi_weight = weight
+        if weight < low_weight:
+            low_weight = weight
 # OUTPUT
-print('\n{}highest{} weight: {}'.format(colors['GREEN'], colors['CLEAN'], hi_weight))
-print('{}Lowest{} weight: {} \n'.format(colors['GREEN'], colors['CLEAN'], low_weight))
+print('\n{}highest{} weight: {}Kg'.format(colors['GREEN'], colors['CLEAN'], hi_weight))
+print('{}Lowest{} weight: {}Kg \n'.format(colors['GREEN'], colors['CLEAN'], low_weight))
