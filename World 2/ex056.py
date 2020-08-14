fc529300435d72e99f39310dedabf653e96d6293
Manoel Fernandes <manoelfelipe2000@hotmail.com>
2020-08-14 13:07:42 -0300

    Update ex056.py

--- World 2/ex056.py
+++ World 2/ex056.py
@@ -4,16 +4,20 @@ colors = {
     'GREEN':'\033[32m'
 }
 # DATA
-people_quant = 6
+people_quant = 4
 avarage = 0
 older_male = 0
 older_male_name = ''
 womans_under20 = 0
 # INPUT
 for i in range(0,people_quant):
-    name = str(input('\n{}Type your name:{} '.format(colors['CLEAN'], colors['GREEN']))).strip()
+    print('='*10)
+    print('{}° Person'.format(i+1))
+    print('='*10)
+    name = str(input('{}Type your name:{} '.format(colors['CLEAN'], colors['GREEN']))).strip()
     age = int(input('{}Type your age:{} '.format(colors['CLEAN'], colors['GREEN'])))
     sex = str(input('{}Type your sex(M/F):{} '.format(colors['CLEAN'], colors['GREEN']))).strip().upper()
+    print('{}'.format(colors['CLEAN']),end='')
     avarage += age
     if age > older_male and sex == 'M':
         older_male = age
@@ -21,6 +25,6 @@ for i in range(0,people_quant):
     if age < 20 and sex == 'F':
         womans_under20 += 1
 # OUTPUT
-print('\nAvarage age of the group: {}{:.2f}{}'.format(colors['GREEN'], (avarage / people_quant), colors['CLEAN']))
+print('\nAvarage age of the group: {}{:.1f}{}'.format(colors['GREEN'], (avarage / people_quant), colors['CLEAN']))
 print('Olderst man in the group: {}{}{}'.format(colors['GREEN'], older_male_name, colors['CLEAN']))
 print('Amount of womans under 20 years: {}{}{}\n'.format(colors['GREEN'], womans_under20, colors['CLEAN']))
