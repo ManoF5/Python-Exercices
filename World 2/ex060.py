3c9f5c05d939fb3f9d2e3c5c477fc1b979c45092
Manoel Fernandes <61895268+ManoF5@users.noreply.github.com>
2020-09-28 19:38:12 -0300

    Update ex060.py

--- World 2/ex060.py
+++ World 2/ex060.py
@@ -1,3 +1,4 @@
+from math import factorial
 # COLORS
 colors = {
     'CLEAN':'\033[m',
@@ -8,11 +9,7 @@ print('='*23)
 print('{} Factorial Calculation {}'.format(colors['GREEN'], colors['CLEAN']))
 print('='*23)
 # INPUT
-n = int(input('Type a number: '))
-factorial = n
+num = int(input('Type a number: '))
+fac = factorial(num)
 # OUTPUT
-i = 1
-while i < n:
-    factorial *=  i
-    i += 1
-print('{}! = {}'.format(n, factorial))
+print('{}! = {}'.format(num, fac))
